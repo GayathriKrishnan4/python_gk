884dd4945fd8054ae9e436ae0bcd3a7c0deaf1fc
GayathriKrishnan4 <gayathrik7008@gmail.com>
2025-09-10 15:26:00 +0530

    Add files via upload

---
+++ pgm7.py
@@ -0,0 +1,10 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+sum=num1+num2
+print(sum)
+diff=num1-num2
+print(diff)
+pro=num1*num2
+print(pro)
+division=num1/num2
+print(division)
