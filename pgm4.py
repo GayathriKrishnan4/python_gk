30c5c4870faccae6ea84bc00bcc91906866f6c28
GayathriKrishnan4 <gayathrik7008@gmail.com>
2025-09-10 15:24:25 +0530

    Add files via upload

---
+++ pgm4.py
@@ -0,0 +1,10 @@
+age=25
+print("type of age",type(age))
+name="rose"
+print("type of name",type(name))
+balance=445.23
+print("balance type",type(balance))
+is_student= True
+print("is_student type", type(is_student))
+fruits=["apple","orange"]
+print("fruits type",type(fruits))
